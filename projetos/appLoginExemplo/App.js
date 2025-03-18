bfe52e62e3634bc1636ba9dc1c4e77b03459e57a
cortezinho <gabrielcortez2802@gmail.com>
2025-03-17 21:54:16 -0300

    Aula 09 atividade03

--- projetos/appLoginExemplo/App.js
+++ projetos/appLoginExemplo/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import App2 from './components/appLoginExemplo';
+import App3 from './components/cadastroScreen';
+
 
 export default function App() {
   return (
