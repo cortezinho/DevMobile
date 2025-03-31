a44103d377411c38aa1e93ff7e51a96b05951407
MykyBalb <mykaelbalbino06@gmail.com>
2025-03-31 19:56:49 -0300

    Ex01_NavBasica

---
+++ Ex01_NavBasica/App.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import Home from "./componentes/home"
+import Settings from "./componentes/settings"
+
+
+const Stack = createNativeStackNavigator()
+
+export default function App() {
+  return(
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name = "Home" component={Home} />
+      <Stack.Screen name = "Settings" component={Settings} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  )
+}
