cbc84510eeefed01889ee128b28f6200bb64cf07
cortezinho <gabrielcortez2802@gmail.com>
2025-05-05 21:51:43 -0300

    aula MAP

---
+++ geoMap/Ex01Localizacao/App.js
@@ -0,0 +1,78 @@
+
+import React, { useState, useEffect } from "react";
+
+import { Text, View } from "react-native";
+
+import * as Location from "expo-location"
+
+import styles from "./styles";
+
+const API_KEY = "";
+
+const URL = `https://maps.google.com/maps/api/geocode/json?key=${API_KEY}&latlng=`;
+
+export default function WhereAmI() {
+
+  const [address, setAddress] = useState("loading...");
+
+  const [longitude, setLongitude] = useState();
+
+  const [latitude, setlatitude] = useState();
+
+  useEffect(() => {
+
+    function setPosition({
+      coords: { latitude, longitude },
+    }) {
+      setLongitude(longitude);
+
+      setlatitude(latitude);
+
+      fetch(`${URL}${latitude},${longitude}`)
+        .then((resp) => resp.json())
+        .then(({ results }) => {
+
+          if (results.length > 0) {
+            setAddress(results[0].formatted_address);
+          }
+        })
+        .catch((error) => {
+
+          console.log(error.message);
+        });
+    }
+
+    let watcher;
+
+
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+
+      setPosition(location);
+
+      watcher = await Location.watchPositionAsync(
+        { accuracy: Location.LocationAccurary.Highest },
+        setPosition
+      )
+    })();
+
+    return () => {
+      watcher?.remove();
+    };
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Address: {address}</Text>
+      <Text style={styles.label}>Latitude: {latitude}</Text>
+      <Text style={styles.label}>Longitude: {longitude}</Text>
+    </View>
+  );
+}
---
+++ geoMap/Ex02MapView/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, StatusBar } from "react-native";
+import MapView from "react-native-maps";
+import styles from "./styles";
+
+StatusBar.setBarStyle("dark-content")
+
+export default () => (
+  <View style={styles.container}>
+    <MapView
+      style={styles.mapView}
+      showsUserLocation={true}
+      followsUserLocation={true}
+    />
+  </View>
+)
